b0a1aaa08e06187b2aafc7bb87ba65ccb1ca6eea
gk <promise_ghostking@163.com>
2025-07-16 22:58:35 +0800

    chore(eslint): 添加 eslint 忽略部分规则

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,9 @@ export default defineConfig([
   pluginVue.configs['flat/essential'],
   {
     files: ['**/*.vue'],
-    languageOptions: { parserOptions: { parser: tseslint.parser } }
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+    rules: {
+      'vue/multi-word-component-names': 'off'
+    }
   }
 ])
