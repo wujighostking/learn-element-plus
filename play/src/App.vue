bcf275385ea1ee0cb0b72b3f8b548794c9c22cf8
gk <promise_ghostking@163.com>
2025-07-18 00:54:31 +0800

    feat(timeline): 添加 timeline 组件部分功能

--- play/src/App.vue
+++ play/src/App.vue
@@ -5,6 +5,7 @@ import Icon from '@/Icon.vue'
 import Button from '@/Button.vue'
 import Link from '@/Link.vue'
 import Alert from '@/Alert.vue'
+import Timeline from '@/Timeline.vue'
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import Alert from '@/Alert.vue'
     <Button></Button>
     <Link></Link>
     <Alert></Alert>
+    <Timeline></Timeline>
   </lay-out>
 </template>
 
