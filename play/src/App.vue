5b9e41ceb9f39b9c5a9aff9feda69e345023ffc2
gk <promise_ghostking@163.com>
2025-08-26 22:05:06 +0800

    chore: 修改 play 页面

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-// import Alert from '@/Alert.vue'
-// import Button from '@/Button.vue'
-// import Calendar from '@/Calendar.vue'
-// import Icon from '@/Icon.vue'
+import Alert from '@/Alert.vue'
+import Button from '@/Button.vue'
+import Calendar from '@/Calendar.vue'
+import Icon from '@/Icon.vue'
 import LayOut from '@/lay-out.vue'
-// import Link from '@/Link.vue'
+import Link from '@/Link.vue'
 import Message from '@/Message.vue'
-// import Switch from '@/Switch.vue'
-// import Timeline from '@/Timeline.vue'
+import Switch from '@/Switch.vue'
+import Timeline from '@/Timeline.vue'
 </script>
 
 <template>
   <LayOut>
-    <!-- <Icon />
+    <Icon />
     <hr>
 
     <Button />
@@ -31,7 +31,7 @@ import Message from '@/Message.vue'
     <hr>
 
     <Switch />
-    <hr> -->
+    <hr>
 
     <Message />
   </LayOut>
